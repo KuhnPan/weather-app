f5a7e1388b965e40d20639e3c5eb369d109068d4
Kuhn Pan <kuhn.pan@gmail.com>
2018-06-04 00:14:06 +0800

    1. Enable HTTPS connection. 2. Add some comments.

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-
 const request = require('request');
+
 // Apply for apiKey via https://openweathermap.org/api
 const apiKey = '06fca7b66b0842d27f5f977c71eb81e3';
 
+const https = require("https"),
+  fs = require("fs");
+
+// Use Let's encrypt certificate and privkey
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/hzjc.tech/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/hzjc.tech/fullchain.pem")
+};
+
+// In order to access css under public directory
 app.use(express.static('public'))
+// To make use of key-value pairs stored on the req-body 
 app.use(bodyParser.urlencoded({ extended: true }));
+// Use EJS(Embedded JavaScript) templating language 
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   res.render('index',{weather: null, error: null})
 })
 
+// Query weather via 3rd part API
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
@@ -32,6 +45,12 @@ app.post('/', function (req, res) {
   });
 })
 
+// Create HTTP Server
 app.listen(80, function () {
   console.log('Weather app listening on port 80!')
+})
+
+// Create HTTPS Server
+https.createServer(options, app).listen(443, function () {
+  console.log('Weather app listening on port 443!')
 })
